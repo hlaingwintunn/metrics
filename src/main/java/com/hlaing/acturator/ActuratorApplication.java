138ea5e6d13c62be6e2bf1adc956c83af91ef05a
hlaingwintun <sv.hlaingwt@gmail.com>
2017-12-11 22:36:43 +0800

    Init commit

---
+++ src/main/java/com/hlaing/acturator/ActuratorApplication.java
@@ -0,0 +1,12 @@
+package com.hlaing.acturator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuratorApplication.class, args);
+	}
+}
